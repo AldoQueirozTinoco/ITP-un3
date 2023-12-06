4e00c200630b57d6d9b75cf66660d02bd432ebf3
AldoQueirozTinoco <aldoqueiroztinoco@gmail.com>
2023-12-06 08:20:50 -0300

    Update main.c
    Feita a funcao de criar tabela

--- main.c
+++ main.c
@@ -1,65 +1,62 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-typedef enum{CHAR,STR,INT,FLOAT,DOUBLE}Types;
+typedef enum { CHAR, STR, INT, FLOAT, DOUBLE } Types;
 
-typedef struct{
+typedef struct {
     char nomecol[20];
     Types tipo;
-}colunas;
+} colunas;
 
-typedef struct{
+typedef struct {
     char ischar;
     char *istring;
     int isint;
     float isfloat;
     double isdouble;
-}Celula;
+} Celula;
 
 typedef struct {
-  char nomeTabela[50];
-  int numLinhas;
-  int numColunas;
-  colunas *colunas; //para nome e types
-  colunas chavePrimaria;
-  Celula **dados;  //matriz de dados
+    char nomeTabela[50];
+    int numLinhas;
+    int numColunas;
+    colunas *colunas; // para nome e types
+    colunas chavePrimaria;
+    Celula **dados; // matriz de dados
 } Tabela;
 
-void creator(){
-    char nometabelas[20];
-    printf("Escolha um nome para a tabela");
-    scanf("%s",nometabelas);
-    strcpy(tabelas[0].nomeTabela,nometabelas);
-    printf("Escolha a quantia de linhas e colunas da tabela");
-    scanf("%d %d",&tabelas[0].numLinhas,&tabelas[0].numColunas);
-    printf("Digite os nomes das colunas da tabela");
-    for(int i=0;i<tabelas[0].numColunas;i++){
-    scanf("%s",nomecolunas);
-    strcpy(tabelas[0].colunas->nomecol,nomecolunas);
-    }
-    printf("Tabela criada com sucesso!");
-}
-int main(){
-    Tabela tabelas[5];
+void creator(Tabela *tabelas) {
     char nometabelas[50];
-    char nomecolunas[50];
-    char chavesprimarias[50];
- 
-/*printf("O QUE DESEJA FAZER?\n1 Criar tabela\n2 Criar linha\n3 Excluir tabela\n4 Procurar registro\n5 Excluir linha\n6 ")
-    nn sei como q implementa isso xd*/
-    
-    printf("Escolha um nome para a tabela");
-    scanf("%s",nometabelas);
-    strcpy(tabelas[0].nomeTabela,nometabelas);
-    printf("Escolha a quantia de linhas e colunas da tabela");
-    scanf("%d %d",&tabelas[0].numLinhas,&tabelas[0].numColunas);
-    printf("Digite os nomes das colunas da tabela");
-    for(int i=0;i<tabelas[0].numColunas;i++){
-    scanf("%s",nomecolunas);
-    strcpy(tabelas[0].colunas->nomecol,nomecolunas);
+    printf("Escolha um nome para a tabela: ");
+    scanf("%s", nometabelas);
+    strcpy(tabelas->nomeTabela, nometabelas);
+    printf("Escolha a quantia de linhas e colunas da tabela: ");
+    scanf("%d %d", &tabelas->numLinhas, &tabelas->numColunas);
+
+    // Aloca dinamicamente o tamanho das colunas
+    tabelas->colunas = (colunas *)malloc(tabelas->numColunas * sizeof(colunas));
+
+    printf("Digite os nomes das colunas da tabela:\n");
+    for (int i = 0; i < tabelas->numColunas; i++) {
+        printf("Coluna %d: ", i + 1);
+        scanf("%s", tabelas->colunas[i].nomecol);
     }
-    printf("Tabela criada com sucesso!");
+
+    printf("Tabela criada com sucesso!\n");
+}
+
+int main() {
+    Tabela tabelas[5];
+
+    creator(&tabelas[0]);
+
+    printf("Nome: %s\n%d Linhas\n%d Colunas de nome(s)\n", tabelas[0].nomeTabela, tabelas[0].numLinhas, tabelas[0].numColunas);
+    printf("Nomes das colunas:\n");
+
+
+    // Liberar memória alocada dinamicamente
+    free(tabelas[0].colunas);
+
     return 0;
 }
-
-
